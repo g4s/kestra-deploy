ca948c9b7895e74c6b17aee93f8e8493f728413f
Gregor Segner <gregor.segner@gmail.com>
2025-07-26 17:58:05 +0200

    feat (shellscript): adding deployment for kestra container

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 VALID_ARGS=$(getopt --long machine-init,deploy)
+KESTRA_CONFIG_URL=""
 
 if [[ $(id -u) -ne 0 ]]; then
     echo "script must be executed with root rights!"
@@ -16,6 +17,7 @@ else
                     ;;
                 --deploy)
                     DEPLOY_SERVICE=true
+                    KESTRA_VERSION=${2:-latest}
                     shift
                     ;;
                 --)
@@ -46,20 +48,43 @@ else
 
             # ensure database password is present
             if [[ $(podman secret exists kestra_db_password) -ne 0 ]]; then
-                echo "Please enter a password for PostgreSQL database:"
-                psqlpw=$(gum input --password)
+                psqlpw=$(gum input --password --placeholder "Enter password for PostgreSQL database [ENTER]")
                 podman secrete create --env=true kestra_db_password psgqlpw
                 unset psqlpw 
             fi
 
+            # deploy database container
             podman run -dt --recreate \
                 --pull=allways \
+                --restart=unless-stopped \
                 -v kestra-postgresql:/var/lib/postgresql/data:Z \
                 -e POSTGRES_DB=kresta \
                 -e POSTGRES_USER=kresta \
                 --secrete kestra_db_password,type=env,target=POSTGRES_PASSWORD \
                 --name kestra-postgresql \
                 docker.io/postgres
+
+            # deploy kestra container
+            if [[ ! -f /etc/kestra/application.yaml ]]; then
+                mkdir -p /etc/kestra
+                curl -fsSL "${KESTRA_CONFIG_URL}" /etc/kestra/application.yaml
+                chmod 0644 /etc/kestra/application.yaml
+            fi
+
+            systemctl enable --now podman.socket
+            mkdir -p /tmp/kestra-wd
+
+            podman run -dt --recreate --privileged \
+                --pull=allways \
+                --restart=unless-stopped \
+                -v /var/run/podman/podman.sock:/var/run/docker.sock \
+                -v kestra-data:/app/storage:Z \
+                -v /tmp/kestra-wd:/tmp/kestra-wd:Z \
+                -v /etc/kestra/application.yml:/etc/config/application.yaml:Z \
+                --publish 8000:8000 \
+                --publish 8001:8001 \
+                kestra/kestra:${KESTRA_VERSION} \
+                server --config /etc/config/application.yaml
         fi
     else
         echo "podman could not be found in path"
